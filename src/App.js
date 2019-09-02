1b32cba94cae523fe5470aa2ef9dfa7b1471be8b
Weyert de Boer <weyert@innerfuse.biz>
2019-09-02 22:16:11 +0100

    fix: build

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src={logo} />
+        <a onClick="javascript:void()">Button</a>
         <a
           className="App-link"
           href="https://reactjs.org"
